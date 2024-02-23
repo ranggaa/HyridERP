0af261032f81e45de3d9c44c63bcc7d220bb9562
MAXHUB <MAXHUB@DESKTOP-9HD1UFT>
2024-02-23 10:59:14 +0530

    New Method class Added

---
+++ Hybrid_Framework/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,28 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=45,b=67;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=678,b=8;
+	int c=a+b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=5678,b=6;
+	int c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=567,b=678;
+	int c=a+b;
+	System.out.println(c);
+}
+}
