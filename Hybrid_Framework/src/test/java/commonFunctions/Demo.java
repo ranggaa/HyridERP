8204508801fa00296fbce9b0a42b2e43940789d3
MAXHUB <MAXHUB@DESKTOP-9HD1UFT>
2024-02-23 11:01:43 +0530

    Values Modified

--- Hybrid_Framework/src/test/java/commonFunctions/Demo.java
+++ Hybrid_Framework/src/test/java/commonFunctions/Demo.java
@@ -3,25 +3,25 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=45,b=67;
+	int a=567,b=544;
 	int c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=678,b=8;
+	int a=234,b=67;
 	int c=a+b;
 	System.out.println(c);
 }
 public void sub()
 {
-	int a=5678,b=6;
+	int a=321,b=6;
 	int c=a+b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=567,b=678;
+	int a=567,b=56;
 	int c=a+b;
 	System.out.println(c);
 }
